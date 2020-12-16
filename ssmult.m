141401c0463c2ddd66b922d154c26e1138ebe63b
Quentin18 <quentindeschamps18@orange.fr>
2020-12-16 18:32:43 +0100

    add help and base choice ssmult

--- ssmult.m
+++ ssmult.m
@@ -1,5 +1,14 @@
-% Shönhage-Strassen algorithm
-function product = ssmult(x, y)
+function product = ssmult(x, y, b)
+% SSMULT  Shönhage-Strassen multiplication algorithm
+%   p = SSMULT(x, y, b) returns the product x * y where x and y are two
+%   integers in base b.
+%
+%   p = SSMULT(x, y) returns the product x * y where x and y are two
+%   integers in base 10.
+
+    if nargin == 2
+        b = 10;
+    end
     % Choose the nearest highest power of 2 of the size of numbers
     max_size = max(numel(int2str(x)), numel(int2str(y)));
     k = pow2(ceil(log2(max_size)));
@@ -12,8 +21,8 @@ function product = ssmult(x, y)
     product = zeros(1, 2^k);
     carry = 0;
     for i = 1 : 2^k
-        product(i) = mod(v(i) + carry, 10);
-        carry = fix((v(i) + carry) / 10);
+        product(i) = mod(v(i) + carry, b);
+        carry = fix((v(i) + carry) / b);
     end
     product = str2double(sprintf('%d', flip(product)));
 end
