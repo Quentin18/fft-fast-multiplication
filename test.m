b8c8f596cd8b1b627167a5491d2d7bd917078e42
Quentin18 <quentindeschamps18@orange.fr>
2020-12-16 18:41:15 +0100

    update tests

--- test.m
+++ test.m
@@ -1,9 +1,9 @@
 % Tests
 format LONG;
-disp(longmult(23958233, 5830, 10));
+disp(longmult(23958233, 5830));
 disp(ssmult(23958233, 5830));
 disp(23958233 * 5830);
 
-disp(longmult(1234567890, 9876543210, 10));
+disp(longmult(1234567890, 9876543210));
 disp(ssmult(1234567890, 9876543210));
 disp(1234567890 * 9876543210);
