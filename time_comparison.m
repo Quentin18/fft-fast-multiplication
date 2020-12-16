a981bbe6e59f754922d49ecacd21f5c46710ae84
Quentin18 <quentindeschamps18@orange.fr>
2020-12-16 18:46:18 +0100

    update time comparison

--- time_comparison.m
+++ time_comparison.m
@@ -1,15 +1,14 @@
 % Measurement of function execution time for multiplication algorithms
-
-nmin = 0;
-nmax = 10^7;
-step = 10^4;
+nmin = 10;
+nmax = 10^6;
+step = 10^3;
 time_longmult = zeros();
 time_ssmult = zeros();
 index = 1;
 for n = nmin:step:nmax
     val = [n n];
     tic;
-    longmult(val(1), val(2), 10);
+    longmult(val(1), val(2));
     time_longmult(index) = toc;
     tic;
     ssmult(val(1), val(2));
@@ -18,7 +17,7 @@ for n = nmin:step:nmax
 end
 
 n_vect = nmin:step:nmax;
-plot(n_vect, time_longmult, n_vect, time_ssmult);
+plot(n_vect, time_longmult, '-.', n_vect, time_ssmult, '-.');
 legend("Long multiplication", "Shönhage-Strassen algorithm");
 title("Comparison of time execution of multiplication algorithms");
 xlabel("Factor");
