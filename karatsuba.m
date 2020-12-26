280d182f1b79cdf580e68a6f8c378b8abfa35fd8
zeng <zeng_ruxue@126.com>
2020-12-26 12:35:50 +0100

    add karatsuba in time_comparison

--- karatsuba.m
+++ karatsuba.m
@@ -12,20 +12,28 @@ function result=karatsuba(x, y, b)
         b = 10;
     end
     
+    % If x or y < 10 : general multiplication
     if (x < 10 || y < 10)
         result=x*y;
+        
+    % Karatsuba algo
     else
+       % Calculate the split size m of the numbers
        max_size = max(numel(int2str(x)), numel(int2str(y)));
        m = max_size/2;
        
+       % Split x and y about the middle
        x1 = fix(x/(b^m));
        x0 = mod(x, b^m);
        y1 = fix(y/(b^m));
        y0 = mod(y, b^m);
        
+       % 3 calls multiplications to numbers approximately half the size
        z2 = karatsuba(x1, y1, b);
        z1 = karatsuba(x1+x0, y1+y0, b);
        z0 = karatsuba(x0, y0, b);
+       
+       % Result
        result = z2*b^(2*m) + (z1-z2-z0)*b^m + z0;
     end
 end
--- time_comparison.m
+++ time_comparison.m
@@ -4,6 +4,7 @@ nmax = 10^6;
 step = 10^3;
 time_longmult = zeros();
 time_ssmult = zeros();
+time_karatsuba = zeros();
 index = 1;
 for n = nmin:step:nmax
     val = [n n];
@@ -13,12 +14,15 @@ for n = nmin:step:nmax
     tic;
     ssmult(val(1), val(2));
     time_ssmult(index) = toc;
+    tic;
+    karatsuba(val(1), val(2));
+    time_karatsuba(index) = toc;
     index = index + 1;
 end
 
 n_vect = nmin:step:nmax;
-plot(n_vect, time_longmult, '-.', n_vect, time_ssmult, '-.');
-legend("Long multiplication", "Shönhage-Strassen algorithm");
+plot(n_vect, time_longmult, '-.', n_vect, time_ssmult, '-.', n_vect, time_karatsuba, '-.');
+legend("Long multiplication", "Shönhage-Strassen algorithm", "Karatsuba algorithm");
 title("Comparison of time execution of multiplication algorithms");
 xlabel("Factor");
 ylabel("Time (in seconds)");
